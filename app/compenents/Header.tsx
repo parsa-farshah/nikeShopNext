2f4b63410d93da8783eb7c299678bd37b14bbb25
parsa <parsafarashah@gmail.com>
2025-12-27 13:18:49 +0330

    add search for mobile size

--- app/compenents/Header.tsx
+++ app/compenents/Header.tsx
@@ -141,7 +141,12 @@ function Header() {
         <div>
           <ModeToggle />
         </div>
-        <div className="px-2 cursor-pointer hover:border-b hover:border-b-black pb-1 duration-300">
+        <div
+          onClick={() => {
+            setWearchWrapper(true);
+          }}
+          className="px-2 cursor-pointer hover:border-b hover:border-b-black pb-1 duration-300"
+        >
           <SearchIcon className="w-5 h-5 mr-2 " />
         </div>
         <div className="px-2 cursor-pointer hover:border-b hover:border-b-black pb-1 duration-300">
